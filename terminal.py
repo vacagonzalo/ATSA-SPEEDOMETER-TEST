b8b4fdee183fb7e8f71ad88f8049ada129da1a3e
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-12-01 15:57:32 -0300

    VT0.1.0

--- terminal.py
+++ terminal.py
@@ -1,10 +1,26 @@
 from serial import Serial
+from datetime import datetime
 
 
 def main():
+    DISTANCE = 2.4
+    coilA = datetime.now()
+    coilB = datetime.now()
     with Serial('/dev/ttyUSB1', 9600, timeout=0.02) as s:
-        msg = s.readline()
-        print(msg, end='\r')
+        while(True):
+            msg = s.readline()
+            if(msg == b'A'):
+                pass
+            elif(msg == b'B'):
+                coilA = datetime.now()
+            elif(msg == b'C'):
+                coilB = datetime.now()
+                speed = DISTANCE / (coilB - coilA)
+                print(f'@{coilB}: {speed}km/h')
+            elif(msg == b'D'):
+                pass
+            else:
+                print("msg:", msg.decode('utf-8'))
 
 
 if __name__ == "__main__":
